3f2923a57378d643f740e10685ee2cc904b18778
23PhamThiPhuongThanh <thanhptphe173367@fpt.edu.vn>
2025-03-14 03:27:21 +0700

    View the list of interns of a project

--- back-end/src/routes/InternRouter.js
+++ back-end/src/routes/InternRouter.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const internController = require('../controllers/InternController')
-const { authInternMiddleware } = require('../middleware/authMiddleware');
+const { authInternMiddleware, authMentorMiddleware } = require('../middleware/authMiddleware');
 
 router.post("/create-intern", internController.createIntern);
 router.post("/create-report", authInternMiddleware, internController.createReport);
+// router.get("/get-interns-by-project/:project_id", authMentorMiddleware, internController.getInternsByProject);
+router.get("/get-interns-by-project/:project_id", internController.getInternsByProject);
 
 module.exports = router;
 
